df224b245cf4f80062acf42656e2511d8a949f97
Sakshi Sharma <SakshiSharma2002@users.noreply.github.com>
2023-03-19 23:00:38 +0530

    Updates

---
+++ precache-manifest.3d96795eed2e5d9545c0b6090c95a53e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c76ef64fc17716497aac",
+    "url": "/Contact-App/static/css/main.e96cdc80.chunk.css"
+  },
+  {
+    "revision": "c76ef64fc17716497aac",
+    "url": "/Contact-App/static/js/main.c76ef64f.chunk.js"
+  },
+  {
+    "revision": "ba1573f026a23319da81",
+    "url": "/Contact-App/static/js/1.ba1573f0.chunk.js"
+  },
+  {
+    "revision": "d320dbcf6b315ba103a1",
+    "url": "/Contact-App/static/js/runtime~main.d320dbcf.js"
+  },
+  {
+    "revision": "6f6bbb16aec97391aefe120ec5a4e6a2",
+    "url": "/Contact-App/static/media/user.6f6bbb16.png"
+  },
+  {
+    "revision": "6c5b78a4ee24bcb63341d579a0acbfcf",
+    "url": "/Contact-App/static/media/user.6c5b78a4.jpg"
+  },
+  {
+    "revision": "18002e0d6d8c6307b382d0f81807a90f",
+    "url": "/Contact-App/index.html"
+  }
+];
